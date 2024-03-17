7f90a1672c1588a89112d73edda71dd41494cbb0
pmad01 <patrickmadhi@gmail.com>
2024-03-17 22:28:34 +0100

    Backend skeleton

--- app.js
+++ app.js
@@ -1,10 +1,71 @@
+require('dotenv').config()
 const express = require('express');
-const app = express();
 const path = require('path');
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require('mongoose-findorcreate');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.set('view engine', 'ejs');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'Little secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/waterfilterDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String,
+    role: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/NOTDECIDED", //where they will be redirected
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 
 app.get('/', (req, res) => {
