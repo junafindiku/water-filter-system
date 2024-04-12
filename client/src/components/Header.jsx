7542298e2095763b00a675659f2ebbe005cdb5b6
AnitaMjeshtri <amjeshtri21@epoka.edu.al>
2024-04-12 21:11:51 +0200

    react project setup, express start script added

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,3 @@
+//Here you can build app component. For instance a header that will be used in every page can be created as a component
+//and then import it in every page.
+//use tsx for typescript and js or jsx for javascript files
