5c822fb1f8480a4c3e15f7f43c33f5e6bbe50a3f
AnitaMjeshtri <amjeshtri21@epoka.edu.al>
2024-04-25 01:27:09 +0200

    express login back end logic refactored

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,31 +16,61 @@ exports.index = async (req, res) => {
 exports.login = (req, res) => {
 
 	
-	passport.authenticate("local")(req, res, async function () {
-		try {
-			const loggedInUser = await User.findById(req.user._id)
-				.populate('roleId')
-				.exec()
-				.catch(error => {
-					console.error('Error fetching user:', error);
-				});
-			const loggedInUserRole = loggedInUser.roleId.title;
-			const roleRedirectMap = {
-				'admin': 'admin',
-				'sales agent': 'sales-agent'
-			};
-			const redirectPath = roleRedirectMap[loggedInUserRole];
-			if (redirectPath) {
-				res.redirect(redirectPath);
-			} else {
-				res.redirect("/");
-			}
-		} catch (error) {
-			console.error(error);
-			res.redirect("login");
-		}
+	// passport.authenticate("local")(req, res, async function () {
+	// 	try {
+	// 		const loggedInUser = await User.findById(req.user._id)
+	// 			.populate('roleId')
+	// 			.exec()
+	// 			.catch(error => {
+	// 				console.error('Error fetching user:', error);
+	// 			});
+	// 		const loggedInUserRole = loggedInUser.roleId.title;
+	// 		const roleRedirectMap = {
+	// 			'admin': 'admin',
+	// 			'sales agent': 'sales-agent'
+	// 		};
+	// 		const redirectPath = roleRedirectMap[loggedInUserRole];
+	// 		if (redirectPath) {
+	// 			res.redirect(redirectPath);
+	// 		} else {
+	// 			res.redirect("/");
+	// 		}
+	// 	} catch (error) {
+	// 		console.error(error);
+	// 		res.redirect("login");
+	// 	}
 
-	});
+	// });
+
+  passport.authenticate("local")(req, res, async function (){
+    try{
+      const loggedInUser = await User.findById(req.user._id)
+      .populate('roleId')
+      .exec()
+      .catch(error=>{
+        console.error('Error fetching user:', error);
+        throw new Error('Error fetching user');
+      });
+      
+      if(loggedInUser){
+        res.status(200).json({
+          status: true,
+          message: "User logged in successfully"
+        })
+      }else{
+        res.status(404).json({
+          status: false,
+          message: "User not found"
+        })
+      }
+    }catch(error){
+      console.log(error);
+      res.status(500).json({
+        status: false,
+        message: "Internal server error"
+      })
+    }
+  })
 }
 
 exports.logout = (req, res) => {
